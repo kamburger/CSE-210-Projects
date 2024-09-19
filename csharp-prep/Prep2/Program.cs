6f405480e5e976e782499a4de34e5cca639f5c9e
kamburger <tay21016@byui.edu>
2024-09-19 14:42:46 -0600

    Prep 2 Core Requirements 1 & 2, Separate console write Lines for grade output.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade? ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+        if (percent >= 90)
+        {
+            Console.WriteLine("You got an A");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("You got a D");
+        }
+        else 
+        {
+            Console.WriteLine("You got an F");
+        }
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed the class! ");
+        }
+        else
+        {
+            Console.WriteLine("You failed :(");
+        }
     }
 }
