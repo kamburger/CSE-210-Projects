7a3d81ff87c615264916848827b5f1cbfc0a659a
kamburger <tay21016@byui.edu>
2024-09-24 15:34:51 -0600

    Prep 4 requirements. Integer List maker, finding sum, average, and max.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        do 
+        {
+            Console.Write("Enter a number: ");
+            string answer = Console.ReadLine();
+            number = int.Parse(answer);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        while (number != 0);
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum = sum + num;
+        }
+        Console.WriteLine($"The sum of the numbers is {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average of the numbers is {average}");
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The largest number is {max}");
     }
+
 }
