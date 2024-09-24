1c538a57216da3c6d6df0ebdd1ec3dd2a028b1be
kamburger <tay21016@byui.edu>
2024-09-24 14:48:50 -0600

    While loop and random number generator added to complete requirements for Prep3. Number Guessing Game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,24 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Enter a magic number: ");
+    {   Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+      /*  Console.Write("Enter a magic number: ");
         string answer = Console.ReadLine();
-        int magicNumber = int.Parse(answer);
+        int magicNumber = int.Parse(answer); */
         Console.Write("What is your guess? ");
         string guess = Console.ReadLine();
         int guessNumber = int.Parse(guess);
-        if (guessNumber > magicNumber)
-        {
-            Console.WriteLine("Lower! ");
-        }
-        else if (guessNumber < magicNumber)
-        {
-            Console.WriteLine("Higher! ");
-        }
-        else
-        {
+        while (guessNumber != magicNumber)
+            {if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower! ");
+                Console.Write("Guess again: ");
+                string guessn = Console.ReadLine();
+                guessNumber = int.Parse(guessn);
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher! ");
+                Console.Write("Guess again: ");
+                string guessn = Console.ReadLine();
+                guessNumber = int.Parse(guessn);
+            }}
             Console.WriteLine("Spot on! You guessed it!");
-        }
+
     }
 }
