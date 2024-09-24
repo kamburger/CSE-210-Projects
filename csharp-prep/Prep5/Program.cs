7e58e8f09d8277c29ab732b2def26888cacdd915
kamburger <tay21016@byui.edu>
2024-09-24 17:40:47 -0600

    Prep 5 requirements. Main function calls secondary functions to display welcome, take in username and number, square the number, and display results.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("What is your Username? ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            string num = Console.ReadLine();
+            int favNumber = int.Parse(num);
+            return favNumber;
+        }
+        static int SquareNumber(int number)
+        {
+            int squared = number*number;
+            return squared;
+        }
+        static void DisplayResult(string user, int squaredNumber)
+        {
+            Console.WriteLine($"Hello {user}! The square of your number is {squaredNumber}.");
+        }
+        static void Main()
+        {
+            DisplayWelcome();
+            string userName = PromptUserName();
+            int userNumber = PromptUserNumber();
+            int numberSquared = SquareNumber(userNumber);
+            DisplayResult(userName, numberSquared);
+        }
+        Main();
     }
 }
