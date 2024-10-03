a3d6b08d83012c90bb7ca494f4c7cb442f610ad8
kamburger <tay21016@byui.edu>
2024-10-02 23:45:24 -0600

    Fixed error of no ;

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -6,8 +6,8 @@ public class Resume
     public List<Job> _jobs = new List<Job>();
     public void DisplayResume()
     {
-        Console.WriteLine($"Name: {_name}")
-        Console.WriteLine($"Jobs:");
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
         foreach (Job job in _jobs)
         {
             job.DisplayJob();
