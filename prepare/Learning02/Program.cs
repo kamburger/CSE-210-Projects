cdb312a248fcc50ae2daf3b63b6beb9b5b116632
kamburger <tay21016@byui.edu>
2024-10-02 23:33:55 -0600

    Create Resume Class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,6 +9,11 @@ class Program
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
-        job1.DisplayJob()
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        
     }
 }
