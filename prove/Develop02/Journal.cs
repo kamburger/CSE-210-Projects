2803319a5b9f2edcc9741149ad28549f10b3f1f0
kamburger <tay21016@byui.edu>
2024-10-10 01:08:32 -0600

    Display Journal function and beginning user choice screen

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -3,4 +3,15 @@ namespace ConsoleApp1;
 public class Journal
 {
     List<Entry> _journal = new List<Entry>();
+
+    public voiod DisplayJournal()
+    {
+        foreach (Entry entry in _journal)
+        {
+            entry.DisplayEntry();
+        }
+    }
+    
+
+
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,14 +5,44 @@ class Program
     static void Main(string[] args)
     {
         List<string> prompts = new List<string>;
-        prompts.Add("Who was the most interesting person I interacted with today?")
-        prompts.Add("What was the best part of my day?")
-        prompts.Add("How did I see the hand of the Lord in my life today?")
-        prompts.Add("What was the strongest emotion I felt today?")
-        prompts.Add("If I had one thing I could do over today, what would it be?")
-        prompts.Add("Did I meet someone new?")
-        prompts.Add("How did I serve today?")
-        prompts.Add("What was the hardest part of today?")
-        
+        prompts.Add("Who was the most interesting person I interacted with today?");
+        prompts.Add("What was the best part of my day?");
+        prompts.Add("How did I see the hand of the Lord in my life today?");
+        prompts.Add("What was the strongest emotion I felt today?");
+        prompts.Add("If I had one thing I could do over today, what would it be?");
+        prompts.Add("Did I meet someone new?");
+        prompts.Add("How did I serve today?");
+        prompts.Add("What was the hardest part of today?");
+
+        Journal myJournal = new Journal();
+
+        Console.WriteLine("Type the number to do the following action:");
+        Console.WriteLine("1: Create a new entry");
+        Console.WriteLine("2: Display your Journal Entries");
+        Console.WriteLine("3: Save your Journal to a text file");
+        Console.WriteLine("4: Upload a Journal from a text file");
+        Console.WriteLine("5: Extra credit item");
+        string action = Console.ReadLine();
+        if (action == "1")
+        {
+            Entry newEntry = new Entry();
+
+            DateTime theCurrentTime = DateTime.Now;
+            string dateText = theCurrentTime.ToShortDateString();
+            newEntry._date = dateText;
+
+            Random randomIndex = new Random();
+            int index = randomIndex.Next(prompts.Count());
+            newEntry._prompt = prompts[index];
+            Console.WriteLine($"{prompts[index]}");
+            string promptAnswer = Console.ReadLine();
+            newEntry._answer = promptAnswer;
+
+            myJournal.Add(newEntry);
+        }
+        else if (action == "2")
+        {
+            myJournal.DisplayJournal()
+        }
     }
 }
