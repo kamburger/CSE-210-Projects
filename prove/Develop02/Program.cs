38d3737b9825c90634ff7cec817960e345861414
kamburger <tay21016@byui.edu>
2024-10-09 19:58:11 -0600

    Moved Entry to correct folder and created list of prompts

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<string> prompts = ;
+        List<string> prompts = new List<string>;
+        prompts.Add("Who was the most interesting person I interacted with today?")
+        prompts.Add("What was the best part of my day?")
+        prompts.Add("How did I see the hand of the Lord in my life today?")
+        prompts.Add("What was the strongest emotion I felt today?")
+        prompts.Add("If I had one thing I could do over today, what would it be?")
+        prompts.Add("Did I meet someone new?")
+        prompts.Add("How did I serve today?")
+        prompts.Add("What was the hardest part of today?")
+        
     }
 }
