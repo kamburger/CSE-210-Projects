38bf89eecd9cf600caa64c58bc338eb464bc800a
kamburger <tay21016@byui.edu>
2024-12-12 08:37:40 -0700

    Fixed spelling error

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Diagnostics.SymbolStore;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation3 World!");
+        
     }
 }
